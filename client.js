69dddf481515f81add0a2d7e343345ff7c2ab1b7
Joshua Oren <joshuadavidoren@gmail.com>
2021-06-19 10:35:09 -0500

    working append

--- client.js
+++ client.js
@@ -3,4 +3,30 @@ $(document).ready(connected)
 
 function connected() {
     console.log('JQ')
+    $('#theSubmitBuatton').on('click', swearToMe)
+}
+
+function swearToMe() {
+    const el = $('.container')
+
+    el.append(` <div class="row align-items-center">
+                <div class="col">
+                    One of three columns
+                </div>
+                <div class="col">
+                    One of three columns
+                </div>
+                <div class="col">
+                    One of three columns
+                </div>
+                <div class="col">
+                    One of four columns
+                </div>
+                <div class="col">
+                    One of five columns
+                </div>
+                <div class="col">
+                    <button id=theDeleteBuatton>Delete</button>
+                </div>
+            </div>`)
 }

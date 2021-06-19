9da5521d9143ba6b25422f5ace02879329b9f175
Joshua Oren <joshuadavidoren@gmail.com>
2021-06-19 12:12:33 -0500

    inputs append, working to math and append monthy total

--- client.js
+++ client.js
@@ -1,32 +1,43 @@
-console.log('js')
-$(document).ready(connected)
+console.log('js');
+$(document).ready(connected);
 
 function connected() {
-    console.log('JQ')
-    $('#theSubmitBuatton').on('click', swearToMe)
+    console.log('JQ');
+    $('#theSubmitBuatton').on('click', swearToMe, howMuch);
+
 }
 
 function swearToMe() {
-    const el = $('.container')
+    const el = $('.container2')
 
     el.append(` <div class="row align-items-center">
+                <div class="col">` +
+        $('#firstName').val() +
+        `</div>
+                <div class="col">` + $('#lastName').val() +
+        `</div>
+                <div class="col">` + $('#id').val() +
+        `</div>
+                <div class="col">` + $('#title').val() +
+        `</div>
+                <div class="col">` + $('#annualSalary').val() +
+        `</div>
                 <div class="col">
-                    One of three columns
+                    <button class=Delete id=theDeleteBuatton>Delete</button>
                 </div>
-                <div class="col">
-                    One of three columns
-                </div>
-                <div class="col">
-                    One of three columns
-                </div>
-                <div class="col">
-                    One of four columns
-                </div>
-                <div class="col">
-                    One of five columns
-                </div>
-                <div class="col">
-                    <button id=theDeleteBuatton>Delete</button>
-                </div>
-            </div>`)
+            </div>`);
+    $('#firstName').val('')
+    $('#lastName').val('')
+    $('#id').val('')
+    $('#title').val('')
+    $('#annualSalary').val('')
+
 }
+
+function howMuch() {
+    console.log('is it working')
+    let el = $('#monthly');
+    el.empty();
+    el.append('text');
+}
+//let moneyMath = ('Total Monthly: $ ' + $('#annualSalary').val() / 1);
